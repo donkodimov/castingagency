337b384a6fd4e0964dcf7690ac59a9c1f30afe09
Donko Dimov <donko@abv.bg>
2023-01-05 16:10:20 +0100

    feat: Add code for frontend

--- backend/flaskr/static/app.js
+++ backend/flaskr/static/app.js
@@ -1,162 +1,36 @@
-/* const checkboxes = document.querySelectorAll(".check-completed");
-for (let i = 0; i < checkboxes.length; i++) {
-  const checkbox = checkboxes[i];
-  checkbox.onchange = function (e) {
-    console.log("event", e);
-    const newCompleted = e.target.checked;
-    const todoId = e.target.dataset["id"];
-    fetch("/todo/" + todoId + "/set-completed", {
-      method: "POST",
-      body: JSON.stringify({
-        completed: newCompleted,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(function () {
-        document.getElementById("errors-todos").className = "hidden";
-        window.location.reload(true);
-      })
-      .catch(function (err) {
-        console.log(err);
-        document.getElementById("errors-todos").className = "";
+window.addEventListener('load', function() {
+  // Get the current URL
+  let url = window.location.href;
+
+  // Extract the access token from the URL
+  let params = new URLSearchParams(url.split('#')[1]);
+  let accessToken = params.get('access_token');
+  let endpoint = document.getElementById('endpoint')
+
+
+  const apiButton = document.getElementById('call-api');      
+  apiButton.addEventListener('click', async () => {
+      let endpointFin = endpoint.value
+
+      fetch(`${endpointFin}`, {
+          method: 'GET',
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + accessToken
+              },
+      }).then(function(response) {
+          if (response.status === 200) {
+              return response.json()
+              
+          } else {
+              console.log("error")
+          }
+      }).then(function(data) {
+      // The request was successful, so render the data in the output element
+         document.getElementById('output').innerHTML = JSON.stringify(data, null, 2);
+      }).catch(function(error) {
+          console.log(error)
       });
-  };
-} */
-/* const checkboxesList = document.querySelectorAll(".list-completed");
-for (let i = 0; i < checkboxesList.length; i++) {
-  const checkbox = checkboxesList[i];
-  checkbox.onchange = function (e) {
-    console.log("event", e);
-    const newCompleted = e.target.checked;
-    const listId = e.target.dataset["id"];
-    fetch("/lists/" + listId + "/set-completed", {
-      method: "POST",
-      body: JSON.stringify({
-        completed: newCompleted,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then(function () {
-        document.getElementById("errors-lists").className = "hidden";
-        window.location.reload(true);
-      })
-      .catch(function (err) {
-        console.log(err);
-        document.getElementById("errors-lists").className = "";
-      });
-  };
-} */
-const descInputActor = document.getElementById("description-actor");
-const descInputAge = document.getElementById("age-actor");
-const descInputGender = document.getElementById("gender");
-document.getElementById("form-actors").onsubmit = function (e) {
-  e.preventDefault();
-  const desc = descInputActor.value;
-  const age = descInputAge.value;
-  const gender = descInputGender.value;
-  descInputActor.value = "";
-  descInputAge.value = "";
-  descInputGender.value = "";
-  fetch("/actors/create", {
-    method: "POST",
-    body: JSON.stringify({
-      name: desc,
-      age: age,
-      gender: gender
-  /*     active_list: actList, */
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (jsonResponse) {
-      console.log("response", jsonResponse);
-      li = document.createElement("li");
-      li.innerText = desc;
-      document.getElementById("actors").appendChild(li);
-      document.getElementById("errors-actors").className = "hidden";
-      window.location.reload(true);
-    })
-    .catch(function (err) {
-      console.log(err);
-      document.getElementById("errors-todos").className = "";
-    });
-};
-const descInputMovie = document.getElementById("description-movie");
-const relDateInput = document.getElementById("release-date");
-document.getElementById("form-movie").onsubmit = function (e) {
-  e.preventDefault();
-  const title = descInputMovie.value;
-  const relDate = relDateInput.value;
-  descInputMovie.value = "";
-  fetch("/movies/create", {
-    method: "POST",
-    body: JSON.stringify({
-      title: title,
-      release_date: relDate
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (jsonResponse) {
-      console.log("response", jsonResponse);
-      li = document.createElement("li");
-      li.innerText = name;
-      document.getElementById("movies").appendChild(li);
-      document.getElementById("errors-movie").className = "hidden";
-      window.location.reload(true);
-    })
-    .catch(function (err) {
-      console.log(err);
-      document.getElementById("errors-movie").className = "";
-    });
-};
-/* document.addEventListener("click", function(evnt){
-                      console.log(evnt.target.id);
-                      id = evnt.target.id;
-                      fetch('/todo/'+ id, {
-                              method: 'DELETE'
-                          })
-                          .then(function() {
-                              document.getElementById('errors').className = 'hidden';
-                          })
-                          .catch(function(err) {
-                              console.log(err)
-                              document.getElementById('errors').className = '';
-                          })
-                  }); */
-const deleteBtns = document.querySelectorAll(".delete-button-movie");
-for (let i = 0; i < deleteBtns.length; i++) {
-  const btn = deleteBtns[i];
-  btn.onclick = function (e) {
-    const movieId = e.target.dataset["id"];
-    fetch("/movies/" + movieId, {
-      method: "DELETE",
-    }).then(function (response) {
-      e.target.parentNode.remove();
-    });
-  };
-}
-const deleteBtnsAct = document.querySelectorAll(".delete-button-actor");
-for (let i = 0; i < deleteBtnsAct.length; i++) {
-  const btn = deleteBtnsAct[i];
-  btn.onclick = function (e) {
-    const actorId = e.target.dataset["id"];
-    fetch("/actors/" + actorId, {
-      method: "DELETE",
-    }).then(function (response) {
-      e.target.parentNode.remove();
-    });
-  };
-}
+      
+});
+});
